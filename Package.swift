6ba039764bee8f7893d71bfdfd9420e3c5746f6d
Widmer <115965339+luizwidmer@users.noreply.github.com>
2026-08-12 15:13:54 -0300

    Pin adversarially hardened Noctweave revision

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ if let localPath = ProcessInfo.processInfo.environment["NOCTWEAVE_PACKAGE_PATH"]
 } else {
     noctweaveDependency = .package(
         url: "https://github.com/luizwidmer/Noctweave.git",
-        revision: "8912862d49f10c8bd307078ba0f05dc021fea1f5"
+        revision: "41a874fc68dc87898f7406b23d290b308364442b"
     )
     noctweavePackageIdentity = "Noctweave"
 }
